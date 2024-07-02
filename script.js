3fc7827e1500f4362d1eae1264e51758c98e464c
Henrique Borgo <henriqueborgo@hotmail.com>
2024-07-02 22:52:45 +0100

    Updated the JS/HTML code to receive the videoID from a JSON local file

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 "use strict";
 
+//Read JSON file (currently only works in Google Chrome)
+import videoInformation from "./videoInformation.json" with { type: "json" };
+
 //Elements
 const btnPositive = document.querySelector(".positive-button");
 const btnNegative = document.querySelector(".negative-button");
 const video = document.querySelector("#video");
 const form = document.querySelector("form");
-let videoID = "i9164jc-j_M";
+
+//Variables
 let userResponse = 0;
 const autoPlay = "?autoplay=1";
+const videoID = videoInformation.id
+
+//Start the page with the desired video
+let videoSrc = `https://www.youtube.com/embed/${videoID}`;
+video.src = videoSrc;
 
 //Prevent the page to refress when clicking button
-//Prevenet default (post/get method) behavior
 form.addEventListener("submit", function (event) {
   event.preventDefault;
 });
@@ -18,7 +26,8 @@ form.addEventListener("submit", function (event) {
 //Function when click the POSITIVE button
 btnPositive.addEventListener("click", function () {
   userResponse = 1;
-  const videoSrc = `https://www.youtube.com/embed/${videoID}${autoPlay}`;
+  //Play the video
+  videoSrc = `https://www.youtube.com/embed/${videoID}${autoPlay}`;
   video.src = videoSrc;
 });
 
